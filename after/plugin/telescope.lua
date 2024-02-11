8c1d20ab2274e932f05709cc9e293eb1b3707d5a
Oleh-Rudko <oleg.rudko@acuityppm.com>
2024-02-11 21:25:53 +0200

    Oleh-Rudko | update grep command

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -38,22 +38,48 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
 
-vim.keymap.set('n', '<leader>a', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") });
+-- open Grep in the Telescope window
+vim.keymap.set('n', '<leader>A', function()
+  builtin.live_grep();
 end)
 
--- in this case it'll open the Telescope
--- local grep_word_under_cursor = function()
---   local word = vim.fn.expand("<cword>")
---   vim.fn.feedkeys(vim.api.nvim_replace_termcodes(":Telescope grep_string search=" .. word .. "<CR>", true, false, true), "n")
--- end
+local exclude_dirs = "--exclude-dir=node_modules --exclude-dir=dist --exclude-dir=build --exclude-dir=.git --exclude-dir=.cache --exclude-dir=public --exclude-dir=vendor --exclude-dir=log --exclude-dir=tmp"
+local exclude_files="--exclude='*.dump'"
 
--- vim.keymap.set("n", "<leader>k", grep_word_under_cursor, { noremap = true, silent = true })
+-- open Grep in the copen default vim window
+vim.keymap.set('n', '<leader>a', function()
+  local input = vim.fn.input("Ack $word $types(,): ")
+  -- Split the input into search term and file types.
+  local input_parts = vim.split(input, " ", {trimempty = true})
+  local search_term = input_parts[1]
+  local file_types = {}
+  if #input_parts > 1 then
+    file_types = vim.split(input_parts[2], ",", {trimempty = true})
+  end
+
+  if search_term ~= "" then
+    -- Start constructing the grep command.
+    local grep_command = "silent grep -R " .. exclude_dirs .. " " .. exclude_files .. " "
+    -- Add include options for each specified file type.
+    for _, ext in ipairs(file_types) do
+      -- Ensure proper escaping for the shell that Vim uses
+      grep_command = grep_command .. "--include=\\*." .. ext .. " "
+    end
+    -- Append the search term, properly escaped.
+    grep_command = grep_command .. vim.fn.shellescape(search_term) .. " ."
+    -- Execute the command.
+    vim.cmd(grep_command)
+    -- Open the quickfix list to show the results.
+    vim.cmd("copen")
+  end
+end, {noremap = true, silent = true})
 
 local grep_word_under_cursor = function()
   local word = vim.fn.expand("<cword>")
+  -- Start constructing the grep command.
+  local grep_command = "silent grep -R " .. exclude_dirs .. " " .. exclude_files .. " "
 
-  vim.cmd("silent grep -R " .. word .. " *")
+  vim.cmd(grep_command .. word .. " *")
   vim.cmd("copen")
 end
 
