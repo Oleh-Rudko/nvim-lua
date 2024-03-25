e09f4d52d24254ae97e8453060a5bbe4d531028b
Oleh-Rudko <oleg.rudko@acuityppm.com>
2024-03-25 08:00:37 +0200

    Oleh-Rudko | ad max tokens

--- after/plugin/chatgpt.rc.lua
+++ after/plugin/chatgpt.rc.lua
@@ -153,7 +153,7 @@ chatgpt.setup({
         model = "gpt-4-0125-preview",
         frequency_penalty = 0,
         presence_penalty = 0,
-        max_tokens = 300,
+        max_tokens = 3000,
         temperature = 0,
         top_p = 1,
         n = 1,
